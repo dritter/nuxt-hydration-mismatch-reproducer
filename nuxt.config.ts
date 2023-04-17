49d204af6cb469be9e19e45ae7a0e47e335eea72
Dominik Ritter <dominik.ritter@chefkoch.de>
2023-04-17 14:27:56 +0200

    Mark my-component as custom element in vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  vue: {
+    compilerOptions: {
+      isCustomElement: (tag) => {
+        return tag === 'my-component'
+      }
+    }
+  }
 })
